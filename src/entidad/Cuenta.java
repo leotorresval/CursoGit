74a21b43c32451bb0a79ec1190d3a7872986216e
leotorresval <leotorresval92@gmail.com>
2023-05-30 16:27:39 -0500

    Primer commit

---
+++ src/entidad/Cuenta.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidad;
+
+/**
+ *
+ * @author User
+ */
+public class Cuenta {
+    private int id;
+    private TipoCuenta tipo;
+    private double saldo;
+    private String cedula;
+
+    public Cuenta(int id, TipoCuenta tipo, double saldo, String cedula) {
+        this.id = id;
+        this.tipo = tipo;
+        this.saldo = saldo;
+        this.cedula = cedula;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TipoCuenta getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCuenta tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+    
+}
